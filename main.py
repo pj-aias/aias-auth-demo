591e6b5eb56527a53b0cbf0dbaba93ebb749e32b
kino-ma <kino.ma.ms@gmail.com>
2020-09-03 02:16:48 +0900

    [change] veriable names

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ from ctypes import cdll, c_char_p, c_int32
 def main():
     lib = cdll.LoadLibrary("aias-core/ffi/target/release/liblib.dylib")
 
-    a = c_char_p(b"hoge")
-    b = c_char_p(b"fuga")
-    c = c_char_p(b"piyo")
-    d = c_char_p(b"foo")
+    signature = c_char_p(b"hoge")
+    message = c_char_p(b"fuga")
+    signer_pubkey = c_char_p(b"piyo")
+    judge_pubkeys = c_char_p(b"foo")
 
     lib.verify.restype = c_int32
 
-    lib.verify(a, b, c, d)
+    res = lib.verify(signature, message, signer_pubkey, judge_pubkeys)
+
+    print(f"output: {res}")
 
 if __name__ == "__main__":
     main()
