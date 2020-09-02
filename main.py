9802819a8375d5c4c924584c72cb61c4a7e9203f
kino-ma <kino.ma.ms@gmail.com>
2020-09-03 04:41:13 +0900

    [add] Flask Server to verify signature

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import platform
+import base64
+import json
 
 import ctypes
 from ctypes import cdll, c_char_p, c_int32
 
-def main():
+from flask import Flask, request, json, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/verify', methods=['POST'])
+def verify():
     lib = get_lib()
 
-    with open("parameters/signature.txt", 'rb') as f:
-        signature = f.read()
+    print(type(request.json))
+    signature_json = request.json['fbs_signature']
+    print(type(signature_json))
+    signature = json.dumps(signature_json).encode('utf-8')
+    print(type(signature))
+
     with open("parameters/message.txt", 'rb') as f:
         message = f.read()
     with open("parameters/signer_pubkey.pem", 'rb') as f:
@@ -17,9 +29,11 @@ def main():
 
     lib.verify.restype = c_int32
 
+    lib.verify.argtypes = (c_char_p, c_char_p, c_char_p, c_char_p)
     res = lib.verify(signature, message, signer_pubkey, judge_pubkey)
+    res = bool(res)
+    return f"Verify result: {str(res)}"
 
-    print(f"verify result: {bool(res)}")
 
 def get_lib():
     pf = platform.system()
@@ -27,6 +41,11 @@ def get_lib():
         return cdll.LoadLibrary("aias-core/ffi/target/release/liblib.dylib")
     elif pf == 'Linux':
         return cdll.LoadLibrary("aias-core/ffi/target/release/liblib.so")
+
+
+def main():
+    app.run()
+
 
 
 if __name__ == "__main__":
