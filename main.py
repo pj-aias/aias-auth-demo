2d33e34ed0a4a03bc20c90d4f1fb6d9beb13135c
kino-ma <kino.ma.ms@gmail.com>
2020-09-03 02:14:42 +0900

    [update] To call Rust function from Python

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+import ctypes
+from ctypes import cdll, c_char_p, c_int32
+
 def main():
-    print("hello world")
+    lib = cdll.LoadLibrary("aias-core/ffi/target/release/liblib.dylib")
+
+    a = c_char_p(b"hoge")
+    b = c_char_p(b"fuga")
+    c = c_char_p(b"piyo")
+    d = c_char_p(b"foo")
+
+    lib.verify.restype = c_int32
+
+    lib.verify(a, b, c, d)
 
 if __name__ == "__main__":
     main()
